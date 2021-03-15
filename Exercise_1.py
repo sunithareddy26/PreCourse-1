dc11dba3340b8e1a18c652c01653f1a72f78c88b
Sunitha Naram Reddy <sunithareddy@fb.com>
2021-03-14 19:28:17 -0700

    Precourse Exercise_1 completed

--- Exercise_1.py
+++ Exercise_1.py
@@ -2,17 +2,25 @@ class myStack:
      def __init__(self):
          self.list = []         
      def isEmpty(self):
-         
+         if len(self.list) <= 0:
+	     return True
+	 else:
+	     return False         
      def push(self, item):
-         
+         self.list.append(item)
+
      def pop(self):
-        
+	 return self.list.pop()        
         
      def peek(self):
+	 return self.list[-1]
         
      def size(self):
+         return len(self.list)
          
      def show(self):
+         for item in self.list:
+             print("List item: %s" % (item ))
          
 
 s = myStack()
